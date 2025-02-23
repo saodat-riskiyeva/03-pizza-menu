73430082106e696de56f7009dc10a8a6b4ea484b
Saodat <saodat.riskiyeva@gmail.com>
2025-02-22 21:01:29 -0500

    setting classes and text conditionally

--- starter/src/index.js
+++ starter/src/index.js
@@ -124,15 +124,16 @@ function Menu() {
 }
 
 function Pizza({ pizzaObject }) {
-  if (pizzaObject.soldOut) return null;
+  // if (pizzaObject.soldOut) return null;
 
   return (
-    <li className="pizza">
+    <li className={`pizza${pizzaObject.soldOut ? " sold-out" : ""}`}>
       <img src={pizzaObject.photoName} alt={pizzaObject.name} />
       <div>
         <h3>{pizzaObject.name}</h3>
         <p>{pizzaObject.ingredients}</p>
-        <span>{pizzaObject.price}</span>
+        {}
+        <span>{pizzaObject.soldOut ? "SOLD OUT" : pizzaObject.price}</span>
       </div>
     </li>
   );
